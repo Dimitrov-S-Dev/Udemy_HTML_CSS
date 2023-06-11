da9f2a549b4f06a23409b985c3a19478e9b6d769
Stan <dimitrov.s.dev@gmail.com>
2023-06-11 22:23:18 +0300

    Udemy_HTML_CSS

---
+++ Javascript_Basics/2.Data_Types/script.js
@@ -0,0 +1,25 @@
+/*
+    We can have many types of data.
+    This includes string, number, boolean, object, and undefined.
+*/
+let full_name = "Carabas";
+let age = 36;
+let height = 5.8;
+let is_prorammer = true;
+let fav_language = null;
+let lunch;
+
+// We can use the typeof operator to find the data type of variable.
+console.log(full_name + " is" +  " a " + typeof(full_name))
+
+// We can create our own objects and set attributes to the object.
+
+
+
+// We can create arrays and call specific array methods.
+
+
+//We can add elements to the array with the .push() method.
+
+
+//We can remove elements from the array with the .pop() method.
