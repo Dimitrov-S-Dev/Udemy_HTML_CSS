88823e6a25bb087f625c7d35e741ecc5c0552d94
Stan <dimitrov.s.dev@gmail.com>
2023-06-17 23:46:21 +0300

    Udemy_HTML_CSS

---
+++ Javascript_DOM/5.Getting_Values_Part2/script.js
@@ -0,0 +1,26 @@
+const color_form = document.querySelector("#color_form")
+const color_btn = document.querySelector("#color_btn")
+const color_message = document.querySelector("#color_message")
+
+//Getting values from linked checkboxes within a form.
+
+function getColor(e){
+    let fav_colors = []
+    for (let i=0; i < color_form.length; i++){
+        if (color_form[i].checked){
+            fav_colors.push(color_form[i].value)
+        }
+    }
+
+    color_message.innerHTML = "You like the following colors " + fav_colors
+}
+
+color_btn.addEventListener("click", getColor)
+//Getting values on the change of an input field
+
+const birthday_input = document.querySelector("#birthday_input")
+const birthday_message = document.querySelector("#birthday_message")
+
+birthday_input.addEventListener("change", function (e){
+    birthday_message.innerHTML = "Your Birthday is recorded as " + this.value
+})
