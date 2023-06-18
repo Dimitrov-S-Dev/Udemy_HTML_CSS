ebc117470cd1d9cb8c3f933ca6db9cc4e3f2f4e1
Stan <dimitrov.s.dev@gmail.com>
2023-06-18 04:24:21 +0300

    Udemy_HTML_CSS

--- Javascript_DOM/6.Getting_Values_from_Forms/script.js
+++ Javascript_DOM/6.Getting_Values_from_Forms/script.js
@@ -1,8 +1,24 @@
+const form_1 = document.querySelector("#form_1")
+const form_2 = document.querySelector("#form_2")
+const form_3 = document.querySelector("#form_3")
+const form_3_message = document.querySelector("#form_3_message")
+
+
 /*
     We can add an event listener directly to a form and listen for a submit event.
     To access the data that is sent via the form we can create a new FormData object attached to THIS form.
 */
+form_1.addEventListener("submit", function (e){
+    alert("You submitted something!")
+    const data = new FormData(this)
+    console.log(data)
+})
 
+form_2.addEventListener("submit", function (e){
+    alert("You have submitted everything!")
+    const data = new FormData(this)
+    console.log(data)
+})
 
 /*
     When a form is submitted the page is reloaded. 
@@ -12,3 +28,18 @@
     Now that we have our FormData object to work with we can use a for loop to loop through the data object.
     We can also use the .get() method and pass in the key which is the name we gave in the form to access the form values.
 */
+
+form_3.addEventListener("submit", function (e){
+    e.preventDefault()
+    const data = new FormData(this)
+    console.log(data)
+
+    for (value of data){
+        console.log(value)
+    }
+
+    console.log(data.get("my_username"))
+
+    form_3_message.innerHTML = "Your username of " + data.get("my_username") + ", password of " + data.get("my_password") + " has been stored"
+
+})
