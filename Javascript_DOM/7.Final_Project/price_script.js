81a77b6b392a755fa3c4ef14fc7f3014e9664a73
Stan <dimitrov.s.dev@gmail.com>
2023-06-19 22:09:03 +0300

    Udemy_HTML_CSS

---
+++ Javascript_DOM/7.Final_Project/price_script.js
@@ -0,0 +1,4 @@
+const reg_input = document.querySelector("#quantity_1")
+const xl_input = document.querySelector("#quantity_2")
+const tool_btn = document.querySelector("#tool_btn")
+const tool_headings = document.querySelectorAll("#tool_values h3")
