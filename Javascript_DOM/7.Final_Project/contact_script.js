1e11e77ae4bdd7e4719eed72fc1d419ed4b6c754
Stan <dimitrov.s.dev@gmail.com>
2023-06-19 22:58:47 +0300

    Udemy_HTML_CSS

--- Javascript_DOM/7.Final_Project/contact_script.js
+++ Javascript_DOM/7.Final_Project/contact_script.js
@@ -11,7 +11,7 @@ contact_form.addEventListener("submit", function (e){
         data.get("my_email") + " around " + data.get("my_time") + ". Talk to you soon!"
     }
     else{
-        contact_message.innerHTML = "Thank you " + data.get("my_name") + "Someone will reach out to you via phone at" +
+        contact_message.innerHTML = "Thank you " + data.get("my_name") + ". Someone will reach out to you via phone at" +
         data.get("my_phone") + " around " + data.get("my_time") + ". Talk to you soon!"
     }
 })
